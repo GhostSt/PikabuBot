7a07914c732ae4febdeb9157513a798fe763aea7
Victor <victor.diditskiy@yandex.ru>
2018-07-28 20:53:23 +0300

    Реализованы команды migration и parser

--- main.go
+++ main.go
@@ -1,132 +1,104 @@
 package main
 
 import (
-	"log"
-
-	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/text/encoding/charmap"
-	"database/sql"
+	cli2 "github.com/urfave/cli"
 	"fmt"
-	"regexp"
+	"os"
+	"log"
 )
 
-type Post struct {
-	postId string
-	author string
-	title  string
-	url    string
-}
-
 func main() {
-	url := "https://pikabu.ru/profile/boss1w"
+	app := cli2.NewApp()
+	app.EnableBashCompletion = true
+	app.Usage = "PikabuBot - tool to collect posts"
+	app.Version = "0.0.1"
+	app.Commands = []cli2.Command{
+		{
+			Name:  "migrations",
+			Usage: "Migration tool",
+			Subcommands: []cli2.Command{
+				{
+					Name:  "migrate",
+					Usage: "Rolls migrations",
+					Flags: []cli2.Flag{
+						cli2.IntFlag{
+							Name: "count, c",
+							Usage: "Rolls certain quantity of migration",
+						},
+					},
+					Action: func(c *cli2.Context) error {
+						println("Migrations migrate")
 
-	setup()
+						return nil
+					},
+				},
+				{
+					Name:  "rollback",
+					Usage: "Rollbacks migrations",
+					Flags: []cli2.Flag{
+						cli2.IntFlag{
+							Name: "count, c",
+							Usage: "Rollbacks certain quantity of migration",
+						},
+					},
+					Action: func(c *cli2.Context) error {
+						println("Migrations rollback")
 
-	doc, err := goquery.NewDocument(url)
+						return nil
+					},
+				},
+			},
+			BashComplete: func(context *cli2.Context) {
+				subCommands := []string{"migrate", "rollback"}
+
+				autoCompleteCommands(subCommands)
+			},
+		},
+		{
+			Name:  "parser",
+			Usage: "Manage parser to parse content and manage data",
+			Subcommands: []cli2.Command{
+				{
+					Name:  "start",
+					Usage: "Starts parser",
+					Action: func(c *cli2.Context) error {
+						println("Starts")
+
+						return nil
+					},
+				},
+				{
+					Name:  "stop",
+					Usage: "Stops parser",
+					Action: func(c *cli2.Context) error {
+						println("Stops parser")
+
+						return nil
+					},
+				},
+			},
+			BashComplete: func(context *cli2.Context) {
+				subCommands := []string{"start", "stop"}
+
+				autoCompleteCommands(subCommands)
+			},
+		},
+	}
+	app.BashComplete = func(context *cli2.Context) {
+		subCommands := []string{"parser", "migrations"}
+
+		autoCompleteCommands(subCommands)
+	}
+
+	err := app.Run(os.Args)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	posts := []Post{}
-
-	doc.Find("div.story").Each(func(i int, s *goquery.Selection) {
-		storyId, _ := s.Attr("data-story-id")
-		author := s.Find("a.story__author").Text()
-		url, _ := s.Find("div.story__header-title a").Attr("href")
-
-		title := s.Find("div.story__header-title").Text()
-		clearTitlePattern := regexp.MustCompile(`[\[\]\(\)\n\t]*`)
-
-		title = clearTitlePattern.ReplaceAllString(title, "")
-
-		var post = Post{
-			storyId,
-			convertWin1251ToUtf8(author),
-			convertWin1251ToUtf8(title),
-			convertWin1251ToUtf8(url),
-		}
-
-		posts = append(posts, post)
-		/**
-		postIDPattern := regexp.MustCompile(`^\d+$`)
-
-		fmt.Println(post.postId)
-		if isPostExists(registry.db, post.postId) {
-			return
-		}
-
-		fmt.Println(post)
-		if postIDPattern.MatchString(post.postId) {
-			savePost(post, registry)
-
-			addedPosts++
-		}
-		*/
-	})
-
-	post := posts[0]
-	savePost(post)
-	fmt.Println(post)
-
-	message := fmt.Sprintf("%s: [%s](%s)", post.author, post.title, post.url)
-	fmt.Println(message)
-
-	res, err := sendMessage(message)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(res)
 }
 
-func convertWin1251ToUtf8(string string) string {
-	returnedString, _ := charmap.Windows1251.NewDecoder().String(string)
-
-	return returnedString
-}
-
-func savePost(post Post)  {
-	db := reg.db
-
-	transaction, err := db.Begin()
-
-	if err != nil {
-		log.Fatal(err)
+func autoCompleteCommands(commands []string) {
+	for _, command := range commands {
+		fmt.Println(command)
 	}
-
-	stmt, err := transaction.Prepare("INSERT INTO post(id, author, title, url) VALUES(?, ?, ?, ?)")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer stmt.Close()
-
-	/**
-	_, err = stmt.Exec(post.postId, post.author, post.title, post.url)
-
-	if err != nil {
-		panic(err)
-	}
-	*/
-	transaction.Commit()
-}
-
-func isPostExists(db *sql.DB, postId string) bool {
-	rows, err := db.Query("SELECT * FROM post WHERE id = ?", postId);
-	if err != nil {
-		panic(err)
-	}
-
-	var post Post
-
-	for rows.Next() {
-		rows.Scan(post.postId, post.author, post.title, post.url)
-
-		return true
-	}
-
-	return false
 }
